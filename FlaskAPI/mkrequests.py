eb5c761465c4eff1bc5b995b0e19fe885859026e
Jiaxun Hou <jiaxun@Jiaxuns-MacBook-Air.local>
2023-03-18 16:12:14 +0800

    create flask api endpoint

---
+++ FlaskAPI/mkrequests.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 14 08:45:56 2020
+
+@author: Ken
+"""
+
+import requests
+from data_input import data_in
+
+URL = 'http://127.0.0.1:8000/predict'
+headers = {"Content-Type": "application/json"}
+data = {"input": data_in}
+
+r = requests.get(URL,headers=headers, json=data)
+#, json=data
+
+r.json()
